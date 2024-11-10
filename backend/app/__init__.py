c416b8b7eac55ef23ce480c45f96f7c03f22e76b
singh13apoorv <singh13.apoorv@gmail.com>
2024-11-10 18:59:42 +0530

    fixed error

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 
+from backend.app.mongo import MongoConnect
+
 # Initialize the Flask app
 app = Flask(__name__)
 
 # Configure your MongoDB connection string (make sure it's in the config)
-app.config["MONGO_URI"] = (
-    "mongodb+srv://singh13apoorv:XETTcALhmWfzBXIx@cluster0.qsk4j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your Mongo URI
-)
-
-# Initialize PyMongo with the Flask app
-mongo = PyMongo(app)
+mongo = MongoConnect()
 
 # Now you can use mongo.db in your other modules for database access

185ebc32d5eb7485ed6000b4f73329aea56eeed3
singh13apoorv <singh13.apoorv@gmail.com>
2024-11-12 04:36:49 +0530

    full code

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,22 @@
-from flask import Flask, g
-from flask_pymongo import pymongo
-
 from app.mongo import MongoConnect
+from flask import Flask, g
+from flask_cors import CORS
 
 
 def create_app(config_name="development"):
     """Create and configure the Flask app."""
 
     # Initialize Flask app
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="frontend/assets")
+
+    # Enable CORS for frontend and backend communication
+    CORS(
+        app,
+        origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+        allow_headers="*",
+        supports_credentials=True,
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    )
 
     @app.before_request
     def before_request():
@@ -25,8 +33,10 @@ def create_app(config_name="development"):
     # Register blueprints (authentication, movies, etc.)
     from app.auth.routes import auth_bp
     from app.movies.routes import movie_bp
+    from app.upload.routes import upload_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(movie_bp, url_prefix="/movies")
+    app.register_blueprint(upload_bp, url_prefix="/upload")
 
     return app
